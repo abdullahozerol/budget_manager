d3b2b66f57fe7e3a9d10cb1c9949b4e15db5740e
Abdullah Özerol <>
2021-04-22 02:43:03 +0300

    Home UI development

---
+++ app/src/main/java/com/ozerol/budgetmanager/FRHome.kt
@@ -0,0 +1,31 @@
+package com.ozerol.budgetmanager
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FRHome : Fragment() {
+
+    companion object {
+        fun newInstance() = FRHome()
+    }
+
+    private lateinit var viewModel: FRHomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fr_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FRHomeViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ozerol/budgetmanager/FRHomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ozerol.budgetmanager
+
+import androidx.lifecycle.ViewModel
+
+class FRHomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
